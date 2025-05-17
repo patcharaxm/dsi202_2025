a83e1f557217a38e63645f6d219341dc73bfe49f
patcharaxm <meaw090647@gmail.com>
2025-05-17 18:14:17 +0700

    update

--- adopt/forms.py
+++ adopt/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import GeneralDonation, SponsorshipDonation, AdoptionRequest
+from .models import GeneralDonation, SponsorshipDonation, AdoptionRequest, MedicalDonation
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class GeneralDonationForm(forms.ModelForm):
     class Meta:
@@ -9,13 +11,19 @@ class GeneralDonationForm(forms.ModelForm):
             'message': forms.Textarea(attrs={'rows': 3}),
         }
 
+
 class SponsorshipDonationForm(forms.ModelForm):
     class Meta:
         model = SponsorshipDonation
         fields = ['donor_name', 'email', 'amount', 'payment_method']
         widgets = {
             'payment_method': forms.RadioSelect,
-            }
+        }
+
+class MedicalDonationForm(forms.ModelForm):
+    class Meta:
+        model = MedicalDonation
+        fields = ['donor_name', 'email', 'amount', 'slip_image']
         
 
 class AdoptionRequestForm(forms.ModelForm):
@@ -28,3 +36,31 @@ class AdoptionRequestForm(forms.ModelForm):
             'property_description': forms.Textarea(attrs={'rows': 3}),
             'motivation': forms.Textarea(attrs={'rows': 3}),
         }
+
+class CustomSignupForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])  # Hash password
+        if commit:
+            user.save()
+        return user
+
+
+class CustomLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
